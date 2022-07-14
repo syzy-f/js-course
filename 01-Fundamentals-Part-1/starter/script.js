45682f23ed0c31da260c58a73be89bfc2380a77c
syzy-f <tymonru@wp.pl>
2022-07-14 16:48:04 +0200

    2-20

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(2 + 2);
 
@@ -9,3 +10,37 @@ let PI = 3.14;
 
 let myFirstJob = "Electrician";
 let myCurrentJob = "Student";
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 2001;
+
+
+const now = 2037;
+const AgeJack = now - 2001;
+console.log(now - AgeJack > now - 2018);
+
+
+const firstName = "Jack";
+const age = "21";
+const jackNew = `I'm ${firstName} and Im ${age} years old`;
+continue.log(`Several
+new
+lines`)
+
+
+const age = 18;
+if (age >= 18) {
+  console.log("He can drive");
+} else {
+  console.log("He cant drive");
+}
+*/
+const inputYear = "1991";
+console.log(Number(inputYear) + 18);
+
+let n = "1" + 1;
+n = n - 1;
+console.assertlog(n);
