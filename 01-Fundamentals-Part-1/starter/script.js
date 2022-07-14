47df7ee90493366ff0b4b7226d2cde565f4483ec
syzy-f <tymonru@wp.pl>
2022-07-14 15:16:22 +0200

    2-10

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,11 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun");
 console.log(2 + 2);
+
+let firstName = "Jack";
+console.log(firstName);
+
+let person = "Jack";
+let PI = 3.14;
+
+let myFirstJob = "Electrician";
+let myCurrentJob = "Student";
